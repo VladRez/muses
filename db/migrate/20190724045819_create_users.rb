3c5e6240de6c9935878ccb781cbc0a82d009fa7d
Vladislav Reznikov <vladrezdev@gmail.com>
2019-07-23 22:01:30 -0700

    update user model

---
+++ db/migrate/20190724045819_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :password_digest
+      t.string :session_token
+      t.integer :counter
+
+      t.timestamps
+    end
+  end
+end
