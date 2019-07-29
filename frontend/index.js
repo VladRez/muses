77e0b532a398ac1690e5c321bdf2bcdee8cabbfc
Vladislav Reznikov <vladrezdev@gmail.com>
2019-07-29 15:49:07 -0700

    update package.json

--- frontend/index.js
+++ frontend/index.js
@@ -3,11 +3,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //Components
 // import Root from './components/root';
-import configureStore from './store/store';
+
 
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
-    const store = configureStore()
+
     ReactDOM.render('test', root)
   });
