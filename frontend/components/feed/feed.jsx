156e6659411f450278f24b464d51b11f1f3ba587
Vladislav Reznikov <vladrezdev@gmail.com>
2019-08-02 10:23:18 -0700

    refactor session login

--- frontend/components/feed/feed.jsx
+++ frontend/components/feed/feed.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import QuestionIndexContainer from './question_index_container'
+import {logout} from '../../util/session_api_util'
+import {Redirect} from 'react-router-dom'
+
+
+
 const Feed = (props) => {
     return (<div className="feed">
+        <button onClick={()=>{logout(); return <Redirect to='/login'/>}}>Logout</button>
        <div className="grid">
        <QuestionIndexContainer/>
        </div>
