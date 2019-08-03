a29af690985996f0ef892659e0be7693014c3033
Vladislav Reznikov <vladrezdev@gmail.com>
2019-08-03 13:28:16 -0700

    add nav header

--- frontend/components/site_header/site_header.jsx
+++ frontend/components/site_header/site_header.jsx
@@ -5,10 +5,30 @@ import { Link } from 'react-router-dom';
 const SiteHeader = (props) => {
 
     const navHead = () => {
-       return (<button onClick={props.logout}>Log Out</button>)
+       return (<div className="siteHeader">
+           <div className="headerInner">
+               <div className="headerLogo">
+                   <a href="/">
+                       <span>Muses</span>
+                   </a>
+                  
+               </div>
+
+            <div className="headerContents">
+            <button onClick={props.logout}>Log Out</button>
+            </div>
+            <div className="headerNav"></div>
+            <div className="rightContents"></div>
+            <div className="searchBar"></div>
+            <div className="headerProfile"></div>
+            <div className="askWrapper"></div>
+           </div>
+       </div>)
     }
     
     return props.currentUser === undefined ? <div></div> : navHead()
 }
 
 export default SiteHeader
+
+//
