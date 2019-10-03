4144bbf56a11dc15cac8276bdbf520dad56c62ac
Vlad Rez <vladrezdev@gmail.com>
2019-10-03 14:27:40 -0700

    update heroku deployment setup

--- config/application.rb
+++ config/application.rb
@@ -10,10 +10,11 @@ module Muses
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    # config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    # config.assets.compile = true
   end
 end
